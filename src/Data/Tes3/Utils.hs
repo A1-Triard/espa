2c7248e3a1d9db11ea25df7653e319c2ce5e8d67
A1 <internalmike@gmail.com>
2017-01-09 11:53:05 +0300

    tes3-0.1.2

---
+++ src/Data/Tes3/Utils.hs
@@ -0,0 +1,18 @@
+module Data.Tes3.Utils
+  ( writeNulledLine
+  , pNulledLine
+  , writeLine
+  , pLine
+  , writeNulledRun
+  , pNulledRun
+  , writeRun
+  , pRun
+  , writeLines
+  , pLines
+  , writeNames
+  , pNames
+  , pT3FileType
+  , pT3Sign
+  ) where
+
+import Data.Tes3.Utils.Native
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,7 @@
 #define TESTS
 #include <haskell>
 import qualified Data.Tes3.Parser.Spec
+import qualified Data.Tes3.Utils.Spec
 
 main :: IO ()
 main = void $ runTestTT tests
@@ -8,4 +9,5 @@ main = void $ runTestTT tests
 tests :: Test
 tests = TestList
   [ Data.Tes3.Parser.Spec.tests
+  , Data.Tes3.Utils.Spec.tests
   ]
